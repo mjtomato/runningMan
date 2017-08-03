5aec7af8ad8a8ddc4fd897ffe8115639e6d07b1b
ljm <562736112@qq.com>
2017-08-03 16:55:07 +0800

    add basis files

---
+++ otherThings.js
@@ -0,0 +1,138 @@
+window.onload = function (argument) {
+	(function(w){
+		var screenW = window.screen.width,
+		    screenH = window.screen.height;
+		var SPEED = 4,
+			COIN_STAY_X = 20,
+			COIN_STAY_Y = 20,
+			COIN_STAY_WIDTH = 30,
+			COIN_STAY_HEIGHT = 30,
+			COIN_SCALE_X = 0.08,
+			COIN_SCALE_Y = 0.08,
+			PER_HEIGHT = screenH/9,
+			PER_WIDTH = screenW/10;
+
+		//地上的石头类
+
+		var Stone = function(x,kind,allImage){
+			this.x = x;
+			this.kind = kind;
+			this.allImage = allImage;
+			this.init();
+		}
+
+		var sp = Stone.prototype;
+
+		sp.init=function(){
+			this.shape = new createjs.Shape();
+			if(this.kind!=="C"){
+				this.h = this.allImage[this.kind].height;
+				this.w = this.allImage[this.kind].width*2;
+				this.y = C_H - this.h;
+				if (this.kind == "A") {
+					this.y = PER_HEIGHT*6;
+					this.h =  PER_HEIGHT*3;
+				}else if (this.kind == "D") {
+					// console.log('D');
+					this.y = PER_HEIGHT*2
+				}else if(this.kind == "E"){
+					// console.log(PER_HEIGHT*3)
+					this.y = PER_HEIGHT*3;
+				}else if(this.kind == "F"){
+					this.y = PER_HEIGHT*4;
+				}
+				this.shape.graphics.beginBitmapFill(this.allImage[this.kind]).drawRect(0, 0, this.w, this.h);
+				this.shape.setTransform(this.x, this.y, 1, 1);
+				// console.log('floor',this.x , this.y)
+			}else {
+				this.h = -1000;
+				this.w = 170;
+				this.y = C_H - this.h;
+				this.shape.graphics.beginFill("#000").drawRect(0, 0, this.w, this.h);
+				this.shape.setTransform(this.x, this.y, 1, 1);
+			}
+			this.shape.visible = false;
+			this.shape.cache(0 , 0 , this.w , this.h);
+			stage.addChild(this.shape);
+		}
+
+		sp.update=function(){
+			this.shape.x -= SPEED;
+		}
+
+		//香蕉类
+		var Coin = function(x , image , kind){
+			this.sizeX = COIN_SCALE_X;
+			this.sizeY = COIN_SCALE_Y;
+			this.kind = kind,
+			this.x = x,
+			this.y = 0,
+			this.isget = false;
+			this.init = function(){
+				// console.log(kind);
+				this.shape = new createjs.Shape();
+				if (this.kind == "1") {
+					// console.log(111);
+					this.y = PER_HEIGHT*1;
+				}else if (this.kind == "2") {
+					this.y = PER_HEIGHT*2;
+				}else if (this.kind == "3") {
+					this.y = PER_HEIGHT*3;
+				}else if (this.kind == "5") {
+					this.y = PER_HEIGHT*5;
+				}
+				// this.y = PER_HEIGHT*1;
+				console.log(this.x,this.y);
+				this.shape.graphics.beginBitmapFill(image).drawRect(0, 0, image.width, image.height);
+				// this.shape.scaleX = COIN_SCALE_X;
+				// this.shape.scaleY = COIN_SCALE_Y;
+				this.shape.setTransform(this.x, this.y, COIN_SCALE_X, COIN_SCALE_Y);
+				this.shape.visible = false;
+				stage.addChild(this.shape);
+
+			}
+			this.init();
+
+			this.update = function(){
+				if(this.isget){
+					this.sizeX = this.sizeX + ((COIN_STAY_WIDTH/image.width) - this.sizeX)*0.2;
+					this.sizeY = this.sizeY + ((COIN_STAY_HEIGHT/image.height) - this.sizeY)*0.2;
+					this.shape.setTransform(
+						this.shape.x + (COIN_STAY_X - this.shape.x)*0.2,
+						this.shape.y + (COIN_STAY_Y - this.shape.y)*0.2,
+						this.sizeX,
+						this.sizeY
+					);
+
+					if(Math.abs(this.shape.x-COIN_STAY_X)<0.5&&Math.abs(this.shape.y-COIN_STAY_Y)<0.5){
+						this.shape.visible = false;
+						this.isget = false;
+						this.sizeX = COIN_SCALE_X;
+						this.sizeY = COIN_SCALE_Y;
+						this.shape.setTransform(0,0,this.sizeX,this.sizeY);
+					}
+				} else{
+					this.shape.x -= SPEED;
+					if(this.shape.x<-image.width*COIN_SCALE_X){
+						this.shape.visible = false;
+					}
+				}
+			}
+
+			this.size = function(){
+				return {
+					w:image.width*COIN_SCALE_X,
+					h:image.height*COIN_SCALE_Y
+				}
+			}
+		}
+
+		w.createCoin = function(x,image , kind){
+			return new Coin(x, image , kind)
+		}
+
+		w.createStone = function(x,kind,allImage){
+			return new Stone(x,kind,allImage);
+		}
+	})(window)
+}
---
+++ person.js
@@ -0,0 +1,140 @@
+(function(w){
+	var screenW = window.screen.width,
+	    screenH = window.screen.height;
+	var FRAME_RATE = 13,	//精灵表播放速度
+		SCALE_X = 0.5,	//X轴缩放
+		SCALE_Y = 0.5,	//Y轴缩放
+		GRAVITY = 15,	//重力加速度
+		JUMP_SPEED = 6,		//垂直速度
+		PROPORTION = 150/1,  //游戏与实际的距离比例
+		PER_HEIGHT = screenH/9,
+		kk = 0;
+		PER_WIDTH = screenW/10;
+		console.log(PER_HEIGHT , PER_WIDTH);
+	var Man = function(x , y , img){
+		this.x = x;
+		this.y = y;
+		this.endy = y;
+		this.vx = 0.5;
+		this.vy = 0;
+		this.ground = [];
+		this.state = "run";
+		this.jumpNum = 0;
+		this.jumpMax = 1;
+		this.init(img);
+	}
+
+	Man.prototype = {
+		constructors:Man,
+
+		init:function(img){
+			var manSpriteSheet = new createjs.SpriteSheet({
+				"images":[img],
+				"frames":{"regX":0,"height":113,"count":45,"regY":1,"width":130},
+				"animations":{
+					"run":{
+						frames:[0,3],
+						next:"run",
+						speed:0.5,
+					}, 
+					"jump":{
+						frames:[3],
+						next:"jump",
+						speed:0.5,
+					},
+					"die":{
+						frames:[4],
+						next:"die",
+						speed:0.5,
+					}
+				}
+			});
+			this.sprite = new createjs.Sprite(manSpriteSheet , this.state);
+			this.sprite.framerate = FRAME_RATE;
+			this.sprite.setTransform(this.x, this.y,(PER_WIDTH*2)/130,(PER_HEIGHT*1)/113);
+			// this.sprite.scaleX = (PER_WIDTH*2)/130;
+			// this.sprite.scaleY = (PER_HEIGHT*1)/113;
+			// console.log(this.sprite.scaleX,this.sprite.scaleY)
+			stage.addChild(this.sprite);
+		},
+
+		update:function(){
+			var sprite = this.sprite;
+			var time = createjs.Ticker.getInterval()/1000;
+			// console.log('外层this.x===',this.x);
+			// console.log('外层sprite.x===',sprite.x);
+			// debugger;
+			if(this.state==="run"){
+				if(sprite.x< (this.x)){
+					sprite.x +=this.vx;
+				}else {
+					sprite.x = this.x;
+				}
+				// debugger;
+			}
+			if(this.endy>sprite.y||this.state==="jump"){
+				var nexty = sprite.y+time*this.vy*PROPORTION;
+				this.vy += time*GRAVITY;
+				sprite.y += time*this.vy*PROPORTION;
+				if(Math.abs(sprite.y-this.endy)<10&&this.vy>0){
+					this.state = "run";
+					this.run()
+					sprite.y=this.endy;
+					this.vy = 0;
+				}
+			}
+			
+			if(sprite.x+(PER_HEIGHT-PER_HEIGHT*SCALE_X)/2<0||sprite.y>C_H+200){
+				this.die();
+				createjs.Ticker.reset();
+				// alert("you Die!");
+				console.log("you are die");
+			}
+
+			switch(this.state){
+				case "run":
+					this.jumpNum = 0;
+					break;
+				case "die":
+					if(sprite.currentFrame===0){
+						sprite.paused = true;
+					}
+				break;
+			}
+		},
+
+		run:function(){
+			this.sprite.gotoAndPlay("run")
+		},
+
+		jump:function(){
+			this.vy = -JUMP_SPEED;
+			this.state = "jump";
+			this.sprite.gotoAndPlay("jump");
+			this.jumpNum++;
+		},
+
+		die:function(){
+			this.state = "die";
+			this.sprite.gotoAndPlay("die")
+		},
+
+		size:function(){
+			return {
+				w:PER_WIDTH,
+				h:PER_HEIGHT
+			}
+		},
+
+		picsize:function(){
+			return {
+				w:PER_WIDTH,
+				h:PER_HEIGHT
+			}
+		}
+	}
+
+	w.createMan = function(x , y , img){
+		return new Man(x , y , img)
+	};
+})(window)
